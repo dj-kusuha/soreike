5046fb99d9b7d68d6836d7087797da825a0b656c
y-kanno <y-kanno@virtualcast.jp>
2022-04-07 19:08:05 +0900

    投稿文をランダムに

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
+/**
+ * Respect for https://zenn.dev/dividebyzero/articles/2815cef7cd446f
+ */
 use dotenv::dotenv;
+use rand::Rng;
 use slack::chat::PostMessageRequest;
 use slack_api::sync as slack;
 use std::env;
 
 fn main() {
     dotenv().unwrap();
+
+    // 投稿文字列を決定する
+    let body = create_body();
+
     let token = env::var("SLACK_BOT_TOKEN").unwrap();
     let channel = format!("#{}", env::var("SLACK_POST_CHANNEL").unwrap());
     let client = slack::default_client().unwrap();
@@ -13,11 +21,34 @@ fn main() {
         &token,
         &PostMessageRequest {
             channel: &channel,
-            text: ":sore_an::ike::anpanman_pan::anpanman_pan::anpanman_an:", // TODO
+            text: &body,
             ..PostMessageRequest::default()
         },
     )
     .unwrap();
 
     println!("{:?}", response.message);
+}
+
+fn create_body() -> String {
+    let dat = [
+        ":sore_an:",
+        ":ike:",
+        ":anpanman_an:",
+        ":anpanman_pan:",
+        ":anpanman_man:",
+    ];
+
+    let mut rng = rand::thread_rng();
+    let mut body = String::from(":sore_an::ike:");
+
+    // 絵文字数の決定 (3～12)
+    let length = rng.gen_range(3..=12);
+    for _ in 0..length {
+        // 絵文字の決定
+        let index = rng.gen_range(0..dat.len());
+        body.push_str(dat[index]);
+    }
+
+    return body;
 }
