3fa92e11aaf7c5938c9ef93bb3a730966140811f
y-kanno <y-kanno@virtualcast.jp>
2022-04-07 16:51:03 +0900

    SLack に投稿するところまで

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use dotenv::dotenv;
+use slack::chat::PostMessageRequest;
+use slack_api::sync as slack;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    dotenv().unwrap();
+    let token = env::var("SLACK_BOT_TOKEN").unwrap();
+    let client = slack::default_client().unwrap();
+    let response = slack::chat::post_message(
+        &client,
+        &token,
+        &PostMessageRequest {
+            channel: "#総合雑談窓",
+            text: ":sore_an::ike::anpanman_an::anpanman_an::anpanman_an:", // TODO
+            ..PostMessageRequest::default()
+        },
+    )
+    .unwrap();
+
+    println!("{:?}", response.message);
 }
